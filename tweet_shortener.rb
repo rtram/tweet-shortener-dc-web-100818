2f1d850aeeb3256b3ea6076fb3d652a7c3ab7cdc
Robin Tram <robin.tram@gmail.com>
2018-09-25 20:40:00 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  key = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+  }
+end 
+
+def word_substituter(tweet)
+  dictionary
+  tweet = tweet.split
+  tweet.collect do |i|
+    if dictionary.key? ("#{i}")
+      "#{dictionary[i]}"
+    else
+      i
+    end 
+  end
+  tweet.join(" ")
+end 
